294d1050f1c0240c0fe3ee4d55e97260106fc424
Lance Hammond Jr <lance.hammondjr@gmail.com>
2021-10-28 18:25:32 -0500

    Lance's Update

--- job-graph/src/component/TheSearch.js
+++ job-graph/src/component/TheSearch.js
@@ -5,10 +5,9 @@ class TheSearch extends React.Component {
   render() {
     return (
       <JobSearch
-        background="https://anima-uploads.s3.amazonaws.com/projects/6178a0ca67f4178eb43821b3/releases/6179e918a070cc6a85bb0479/img/background@1x.png"
+        iconAwesomeJoget="https://anima-uploads.s3.amazonaws.com/projects/6178a0ca67f4178eb43821b3/releases/6179ef69bbae245115aa9af3/img/icon-awesome-joget-1@1x.png"
         listOfJobs="https://anima-uploads.s3.amazonaws.com/projects/6178a0ca67f4178eb43821b3/releases/6179ef69bbae245115aa9af3/img/list-of-jobs@1x.png"
         listOfJobs2="https://anima-uploads.s3.amazonaws.com/projects/6178a0ca67f4178eb43821b3/releases/6179ef69bbae245115aa9af3/img/list-of-jobs@1x.png"
-        iconAwesomeJoget="https://anima-uploads.s3.amazonaws.com/projects/6178a0ca67f4178eb43821b3/releases/6179ef69bbae245115aa9af3/img/icon-awesome-joget-1@1x.png"
         data-id="e16276be-0212-4994-bba1-30bdffe22f85:an-component-instance"
       />
     );
@@ -23,24 +22,13 @@ class JobSearch extends React.Component {
   }
 
   render() {
-    const { background, listOfJobs, listOfJobs2, iconAwesomeJoget } = this.props;
+    const { iconAwesomeJoget, listOfJobs, listOfJobs2 } = this.props;
 
     return (
       <div className="container-center-horizontal">
         <div className="job-search screen" data-id="e16276be-0212-4994-bba1-30bdffe22f85">
-          <div className="body" data-id="279c5dd9-034d-425c-a548-be5e07716fc6">
-            <div className="overlap-group" data-id="an|10clszhu">
-              <div
-                className="background"
-                style={{ backgroundImage: `url(${background})` }}
-                data-id="c43a8683-97fb-4b27-a2fd-a581c03942d9"
-              ></div>
-              <img className="l-ist-of-jobs" data-id="952cd435-d79a-4cf9-9ad5-02b6fe6efed6" src={listOfJobs} />
-              <img className="l-ist-of-jobs-1" data-id="8188f3ac-0f04-4ca0-a884-2a738e492796" src={listOfJobs2} />
-            </div>
-          </div>
           <div className="header" data-id="07687d4a-1497-4a1b-8476-9248d2e26539">
-            <div className="overlap-group1" data-id="an|ju6Fo1al">
+            <div className="overlap-group1" data-id="an|AulN7IcQ">
               <div className="header-1" data-id="ba7577e2-c7e5-4507-bca7-61591352da00"></div>
               <div className="text-field-outlined" data-id="54cbb456-a593-493d-ae9a-cc65281bce62">
                 <label className="mdc-text-field mdc-text-field--outlined">
@@ -63,9 +51,14 @@ class JobSearch extends React.Component {
               />
             </div>
           </div>
+          <div className="body" data-id="279c5dd9-034d-425c-a548-be5e07716fc6">
+            <div className="overlap-group" data-id="an|C2ZCoSuW">
+              <img className="l-ist-of-jobs" data-id="952cd435-d79a-4cf9-9ad5-02b6fe6efed6" src={listOfJobs} />
+              <img className="l-ist-of-jobs-1" data-id="8188f3ac-0f04-4ca0-a884-2a738e492796" src={listOfJobs2} />
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
-
