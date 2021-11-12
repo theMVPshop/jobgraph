3b41c3d1731746e60c918061f129e9075b6a348e
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-11-11 18:51:39 -0600

    added recharts, example displaying

--- job-graph/src/component/TheChart.js
+++ job-graph/src/component/TheChart.js
@@ -1,164 +1,77 @@
-import React from "react";
-import * as mdc from "material-components-web";
-import { Line } from "react-chartjs-2";
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-
-
-
-//Link html with this code below
-
-//#region Script
-//DOM element
-//const element = this.myRef.current;
-//const ctx = element;
-//example db array
-const dataFriend1 = [10, 30, 40, 50, 60, 50, 40, 30, 30, 30, 10, 60, 50];
-const dataFriend2 = [10, 30, 40, 50, 60, 50, 40, 30, 30, 30, 10, 60, 50];
-const dataFriend3 = [10, 30, 40, 50, 60, 50, 40, 30, 30, 30, 10, 60, 50];
-
-//add extra data sets for all locations
-const data = [];
-const data2 = [];
-const data3 = [];
-
-
-//How long it takes for the data to reach the other side
-const totalDuration = 10000;
-const delayBetweenPoints = totalDuration / data.length;
-const previousY = (ctx) =>
-  ctx.index === 0
-    ? ctx.chart.scales.y.getPixelForValue(100)
-    : ctx.chart
-        .getDatasetMeta(ctx.datasetIndex)
-        .data[ctx.index - 1].getProps(["y"], true).y;
-
-//amount of time in (unit of time here). Higher amounts give more data to increase amount of spikes
-//time to display should be the amount of time stamps are available
-var timeToDisplay = 100;
-var startingAmount = 0;
-
-//chart data
-const dataStream = {
-  //this is for the little square that pops up when hovering over lines
-  datasets: [
-    {
-      borderColor: "red",
-      borderWidth: 1,
-      radius: 0,
-      data: data,
-    },
-    {
-      borderColor: "blue",
-      borderWidth: 1,
-      radius: 0,
-      data: data2,
-    },
-    {
-      borderColor: "purple",
-      borderWidth: 1,
-      radius: 0,
-      data: data3,
-    } /*, In case you forget how to add more data
-    {
-      borderColor: 'blue',
-      borderWidth: 1,
-      radius: 0,
-      data: data2,
-    }*/,
-  ],
-};
-//chart options
-const config = {
-  type: "line",
-  options: {
-    maintainAspectRatio: false,
-    responsive: true,
-    animation: {
-      x: {
-        type: "number",
-        easing: "linear",
-        duration: delayBetweenPoints,
-        from: NaN, // the point is initially skipped
-        delay(ctx) {
-          if (ctx.type !== "data" || ctx.xStarted) {
-            return 0;
-          }
-          ctx.xStarted = true;
-          return ctx.index * delayBetweenPoints;
-        },
-      },
-      y: {
-        type: "number",
-        easing: "linear",
-        duration: delayBetweenPoints,
-        from: previousY,
-        delay(ctx) {
-          if (ctx.type !== "data" || ctx.yStarted) {
-            return 0;
-          }
-          ctx.yStarted = true;
-          return ctx.index * delayBetweenPoints;
-        },
-      },
-    },
-    interaction: {
-      intersect: false,
-    },
-    plugins: {
-      legend: false,
-    },
-    scales: {
-      x: {
-        type: "linear",
-      },
-    },
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
   },
-};
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
-//may have to put this somewhere else
-for (let i = 0; i < timeToDisplay; i++) {
-  data.push({ x: i, y: dataFriend1[i] });
-  data2.push({ x: i, y: dataFriend2[i] });
-  data3.push({ x: i, y: dataFriend3[i] });
-}
-
-const LineChart = () => <Line data={dataStream} options={config} />;
-//#endregion
-
-class TheChart extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.myRef= React.createRef();
-  }
-
-  componentDidMount() {
-    new mdc.textField.MDCTextField(
-      document.querySelector(".text-field-outlined .mdc-text-field")
-    );
-  }
+export default class Example extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
 
   render() {
-
-    /*ref={
-              function(myChart) {
-                thisObject.chart = myChart;
-              }
-            } */
-    
-    //const ctx = myChart;
-
     return (
-        <div className="chart-container ">
-          <h1>Chart View</h1>
-          <canvas className="chart-container graph-box-color" id="chart">
-            ref = {this.myRef}
-          </canvas>
-        </div>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     );
   }
-
-  
 }
-
-export default LineChart;
