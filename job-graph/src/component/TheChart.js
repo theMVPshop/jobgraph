7e6f22b6f29a12c9c379eb8fe3ef54354827d0e5
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-12-27 19:51:22 -0600

    filter added

--- job-graph/src/component/TheChart.js
+++ job-graph/src/component/TheChart.js
@@ -11,77 +11,23 @@ import {
   
 } from "recharts";
 
+//filter variables
+let cityNameFilter;
+let jobNameFilter;
+
+//data variables
 let cityName;
-var jobName;
-var dbJobs;
-var dbTimeStamp;
+let jobName;
+let dbJobs = [];
+let dbTimeStamp = [];
 let lineChartData = [];
 
-//#region Stuff that breaks the code when you delete it
-
-// cityName = this.state.jobInfo;
-
-// console.log(cityName);
-// jobName = "Software";
-// cityName = this.state.jobInfo.filter(
-//   (city) => city.job_location === "dallas, tx"
-// );
-
-// jobName = this.state.jobInfo.filter(
-//   (jobTitle) => jobTitle.job_search_term === "software"
-// );
-
-// dbJobs = [
-//   30, 25, 35, 38, 45, 50, 55, 80, 100, 90, 86, 80, 70, 75, 60, 70, 80, 90, 110,
-//   125, 150, 130, 150, 203, 232, 250, 255, 101, 107, 160, 200, 210, 246, 247,
-//   264, 255,
-// ];
-
-dbTimeStamp = [
-  "01-15-2021",
-  "02-15-2021",
-  "03-15-2021",
-  "04-15-2021",
-  "05-15-2021",
-  "06-15-2021",
-  "07-15-2021",
-  "08-15-2021",
-  "09-15-2021",
-  "10-15-2021, 11-15-2021",
-  "12-15-2021",
-  "01-15-2022",
-  "02-15-2022",
-  "03-15-2022",
-  "04-15-2022",
-  "05-15-2022",
-  "06-15-2022",
-  "07-15-2022",
-  "08-15-2022",
-  "09-15-2022",
-  "10-15-2022, 11-15-2022",
-  "12-15-2022",
-  "01-15-2023",
-  "02-15-2023",
-  "03-15-2023",
-  "04-15-2023",
-  "05-15-2023",
-  "06-15-2023",
-  "07-15-2023",
-  "08-15-2023",
-  "09-15-2023",
-  "10-15-2023, 11-15-2023",
-  "12-15-2023",
-];
-
-//DO NOT TOUCH THE CODE BELOW, for someone reason when it is moved, it breaks the code
-
-//putting all the db elements into one array
+//graph variables
+let XAxi;
+let YAxi;
 var dbElement = [cityName, jobName, dbJobs, dbTimeStamp];
+var dbArrayLength;
 
-//amount of index's we are handling (Will be the amount of timestamps)
-var dbArrayLength = dbElement[3].length;
-
-//#endregion
 
 
 export default class LineGraph extends PureComponent {
@@ -93,73 +39,99 @@ export default class LineGraph extends PureComponent {
   }
 
   async componentDidMount() {
-    console.log("Accessing DidMount");
     var returnThis = await this.GetData();
     //returnThis=JSON.parse(returnThis);
     this.setState(returnThis);
   }
-    
-/*
-      TODO
-
-      Find correct format [[X]
-      Query only what we need, search term and location
-      reformulate data to format
-      
-*/
 
 //Fetch the data we need, according to the search terms, job type, and add them to state for further use
   GetData() {
-    console.log("Accessing GetData");
+    //console.log("Accessing GetData");
     axios.get("https://jobsearch-mysql.herokuapp.com/").then((res) => {
       const jobInfo = res.data;
       this.setState({ jobInfo });
+
+      //testing assign filter input
+      /*city options: "houston, tx", "san antonio, tx", "dallas, tx", "austin, tx",
+      "fort worth, tx", "arlington, tx", "plano, tx", "irving, tx", "garland, tx", "frisco, tx", "mckinney, tx" */
+      /*job options: "dental assistant", "medical assistant", "web developer", "software engineer"*/
+      cityNameFilter = "dallas, tx";
+      jobNameFilter = "web developer";
+
+      //filter our data to the correct location and job type
+      this.FilterData();
+      //add filtered data to data variable to graph
       this.AddData();
     });
   }
 
   FilterData(){
+    /*Filter
+    1. City
+    2. Job name
+    3. Jobs
+    4. Time stamps
+    */
 
+
+    cityName = this.state.jobInfo.filter((job) => job.job_location === cityNameFilter);
+    console.log("city name");
+    console.log(cityName);
+
+    jobName = cityName.filter((job) => job.job_search_term === jobNameFilter);
+    console.log("job name");
+    console.log(jobName);
+    console.log("jobName length: " + jobName.length);
+
+    //filter for only our jobName jobs
+    for(let i = 0; i < jobName.length; i++)
+    {
+      //change strings into numbers
+      dbJobs[i] = jobName[i].jobs.replace(/,/g, '');
+      
+    }
+
+    //time stamps
+    for(let i = 0; i < jobName.length; i++)
+    {
+      dbTimeStamp[i] = jobName[i].time_stamp.slice(0, 10);
+    }
+
+    dbElement = [cityName, jobName, dbJobs, dbTimeStamp];
+
+    console.log("jobs:");
+    console.log(dbJobs);
+    console.log("times:");
+    console.log(dbTimeStamp);
   }
 
   AddData(){
-      cityName = this.state.jobInfo.filter((job) => job.job_location === "dallas, tx");
 
-      jobName = this.state.jobInfo.filter((job) => job.job_search_term === "software");
+    dbArrayLength = dbElement[2].length;
 
-      dbJobs = this.state.jobInfo.filter((job) => job.jobs);
-      console.log("jobs:");
-      console.log(dbJobs);
-
-      for(let i = 0; i < dbJobs.length - 1; i++)
-      {
-        dbTimeStamp[i] = dbJobs[i].time_stamp;
-      }
-      
-      dbElement = [cityName, jobName, dbJobs, dbTimeStamp];
-      dbArrayLength = dbElement[3].length;
+    //XAxi = dbArrayLength;
+    //YAxi = dbArrayLength;
 
       for (var i = 0; i < dbArrayLength; i++) {
         lineChartData.push(
           {
-            timeStamp: dbTimeStamp[i].slice(0, 9),
-            Jobs: parseFloat(dbJobs[i].jobs.replace(/,/g, '')),
+            //take only the date from our timestamps
+            timeStamp: dbTimeStamp[i],
+            //print the job amounts as numbers removing the comma
+            //Jobs: parseFloat(dbJobs[i].replace(/,/g, '')),
+            Jobs: dbJobs[i],
           },
         );
      }
-
+    
+     //set our data to state for further use
      this.setState({ lineChartData });
 
      console.log("lineChartData");
      console.log(lineChartData);
   }
 
-  
-
-  
-
   render() {
-    console.log("Accessing render");
     return (
       
       <ResponsiveContainer width="100%" height="100%">
@@ -181,8 +153,8 @@ export default class LineGraph extends PureComponent {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey={this.state.jobInfo.time_stamp} />
-          <YAxis dataKey={this.state.jobInfo.jobs} />
+          <XAxis dataKey={XAxi} />
+          <YAxis dataKey={YAxi} />
           <Tooltip />
           
           <Line type="monotone" dataKey="Jobs" stroke="#1997b5" activeDot={{ r: 8 }} />
